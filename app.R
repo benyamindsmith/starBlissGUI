bbdf05d3c612fb186c52f89d5ff1b55d241c5a76
benyamindsmith <benjamin@alterclimate.com>
2023-03-09 13:55:18 -0500

    some color

--- app.R
+++ app.R
@@ -1,53 +1,10 @@
 library(shiny)
 library(shinyWidgets)
 
-# Function for Making Radio Images
-radioImages <- function(inputId, images, values){
-  radios <- lapply(
-    seq_along(images),
-    function(i) {
-      id <- paste0(inputId, i)
-      tagList(
-        tags$input(
-          type = "radio",
-          name = inputId,
-          id = id,
-          class = "input-hidden",
-          value = as.character(values[i])
-        ),
-        tags$label(
-          `for` = id,
-          tags$img(
-            src = images[i]
-          )
-        )
-      )
-    }
-  )
-  do.call(
-    function(...) div(..., class = "shiny-input-radiogroup", id = inputId), 
-    radios
-  )
-}
-
-radio_css<- HTML(
-  ".input-hidden {",
-  "  position: absolute;",
-  "  left: -9999px;",
-  "}",
-  "input[type=radio] + label>img {",
-  "  width: 50px;",
-  "  height: 50px;",
-  "}",
-  "input[type=radio]:checked + label>img {",
-  "  border: 1px solid #fff;",
-  "  box-shadow: 0 0 3px 3px #45b6fe;",
-  "}"
-)
-
-
 ui <- fluidPage(
-    tags$head(tags$style(radio_css)),
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+      ),
     br(),
     sidebarLayout(
         mainPanel(),
@@ -55,11 +12,11 @@ ui <- fluidPage(
           navbarPage("Create Your Custom Star Map",
                      tabPanel("Size"),
                      tabPanel("Design",
-                              radioImages(
+                              radioGroupButtons(
                                 "radio",
-                                images = c("//./www/black.svg",
-                                           "//./www/green.svg"),
-                                values = c("black", "green")
+                                choiceNames = c('<div class="icon_black"></div>',
+                                                '<div class="icon_green"></div>'),
+                                choiceValues = c("black", "green")
                               ),
                               verbatimTextOutput("test")),
                      tabPanel("Moment"),
